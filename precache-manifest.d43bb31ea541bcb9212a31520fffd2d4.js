3255878d853d14608cfd4e29c7c61cffa7ca8157
krishnadeab <kkrishna@deaninfotech.com>
2019-02-20 10:58:06 +0530

    Updates

---
+++ precache-manifest.d43bb31ea541bcb9212a31520fffd2d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demopage/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "17a7551204dfb40e8981",
+    "url": "/demopage/static/js/runtime~main.17a75512.js"
+  },
+  {
+    "revision": "853aa6dcf3257fcb31e1",
+    "url": "/demopage/static/js/main.853aa6dc.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/demopage/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "853aa6dcf3257fcb31e1",
+    "url": "/demopage/static/css/main.3da94650.chunk.css"
+  },
+  {
+    "revision": "f64f2aefab980abb909367716098596b",
+    "url": "/demopage/index.html"
+  }
+];
